614174f0d579b717b94154ae699e13137358bf2f
Alan Chung <100034245+AlanKLChung@users.noreply.github.com>
2022-08-29 23:01:59 +0800

    t1

---
+++ ACITRoomRJavaDeomo/src/Pi4JOperator/Pi4J00Operator.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class_1.java to edit this template
+ */
+
+package Pi4JOperator;
+
+/**
+ *
+ * @author AlanChung
+ */
+public abstract class Pi4J00Operator {
+
+}
