04d91ac937d4d8a64aabd143d5cf132c75504fe4
Alan Chung <100034245+AlanKLChung@users.noreply.github.com>
2022-11-02 16:24:29 +0800

    Updated on 2022/11/03 for GPIO application 1

---
+++ ACITRoomRJavaDeomo/src/pi4jOperator/pi4j01gpio.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pi4jOperator;
+
+/**
+ *
+ * @author AlanChung
+ */
+public abstract class pi4j01gpio extends pi4j00operator {
+    
+}
